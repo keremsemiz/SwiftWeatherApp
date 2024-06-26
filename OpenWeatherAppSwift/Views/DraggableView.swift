751851003966d5b107585b05e5e14981bca848eb
Kerem Semiz <kerox350@gmail.com>
2024-06-26 15:27:27 +0200

    add draggable feature for detailed weather

---
+++ OpenWeatherAppSwift/Views/DraggableView.swift
@@ -0,0 +1,18 @@
+//
+//  DraggableView.swift
+//  OpenWeatherAppSwift
+//
+//  Created by Kerem Semiz on 26.06.24.
+//
+
+import SwiftUI
+
+struct DraggableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DraggableView()
+}

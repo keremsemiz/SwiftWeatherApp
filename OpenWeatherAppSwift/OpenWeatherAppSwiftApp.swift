842d69ccc04d44447d94efb0d97c66b93d9a0202
Kerem Semiz <kerox350@gmail.com>
2024-06-26 14:11:39 +0200

    Initial Commit

---
+++ OpenWeatherAppSwift/OpenWeatherAppSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherAppSwiftApp.swift
+//  OpenWeatherAppSwift
+//
+//  Created by Kerem Semiz on 26.06.24.
+//
+
+import SwiftUI
+
+@main
+struct OpenWeatherAppSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
